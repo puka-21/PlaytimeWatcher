3fe526d54fd7a88eb25888f101f10679d65b70a6
puka-21 <pikunovilya0709@gmail.com>
2026-09-15 21:07:25 +0500

    Удалил ненужные файлы

--- start_hidden.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM start_hidden.bat
-REM Точка входа для Bloxstrap "Пользовательские интеграции".
-REM Сам .bat откроется на долю секунды (это неизбежно для cmd.exe), но сразу
-REM передаёт запуск в run_hidden.vbs, который уже полностью прячет процесс.
-
-wscript.exe "%~dp0run_hidden.vbs"
